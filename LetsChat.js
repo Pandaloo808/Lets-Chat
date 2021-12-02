2c35b255b00d17a5d0923d83c7604511d1f81a84
Pandaloo808 <93350605+Pandaloo808@users.noreply.github.com>
2021-12-02 16:01:52 -0500

    Update LetsChat.js

--- LetsChat.js
+++ LetsChat.js
@@ -1,1 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDrQjdBjH9s6i1gur1zOgYbbeLUiktr4Zw",
+  authDomain: "emeralds-and-rubies-a1383.firebaseapp.com",
+  databaseURL: "https://emeralds-and-rubies-a1383-default-rtdb.firebaseio.com",
+  projectId: "emeralds-and-rubies-a1383",
+  storageBucket: "emeralds-and-rubies-a1383.appspot.com",
+  messagingSenderId: "119886941310",
+  appId: "1:119886941310:web:73342501fccbf3352b5c2f"
+};
 
+firebase.initializeApp(firebaseConfig);
