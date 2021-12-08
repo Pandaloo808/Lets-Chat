3bd3a7543228185ee4cb2e14dc319e9caf3d85d0
Pandaloo808 <93350605+Pandaloo808@users.noreply.github.com>
2021-12-08 18:11:57 -0500

    Update LetsChat.js

--- LetsChat.js
+++ LetsChat.js
@@ -9,3 +9,8 @@ var firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
+function adduser() {
+  user_name=document.getElementById("user_name").value;
+  localStorage.setItem("user_name",user_name);
+  window.location="kwitter_room.html";
+}
